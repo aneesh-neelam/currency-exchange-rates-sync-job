631559828a612eaea5b9b9a435b261726655cfc1
Aneesh Neelam <neelam.aneesh@gmail.com>
2024-02-10 16:20:48 +0530

    Release 1.6-Prod
    * Added Sentry monitoring

--- main.py
+++ main.py
@@ -183,7 +183,7 @@ if __name__ == '__main__':
     sentry_dsn = get_sentry_dsn()
     sentry_sdk.init(
         dsn=sentry_dsn,
-        # Tracing
+        attach_stacktrace=True,
         enable_tracing=True,
         # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
         traces_sample_rate=1.0,
